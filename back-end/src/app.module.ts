b4962b578c5a0e84d9383383f13cdd246e759c7f
Le Van Hai <lehai@HSVINA.LOCAL>
2025-11-05 09:23:53 +0700

    custom eslint && prettierrc

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,51 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from '@/modules/users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  AcceptLanguageResolver,
+  QueryResolver,
+  HeaderResolver,
+  CookieResolver,
+  I18nModule,
+  I18nJsonLoader,
+} from 'nestjs-i18n';
+import { AuthModule } from '@/modules/auth/auth.module';
+import * as path from 'path';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    I18nModule.forRootAsync({
+      useFactory: () => ({
+        fallbackLanguage: 'vi',
+        loader: I18nJsonLoader,
+        loaderOptions: {
+          path: path.join(process.cwd(), 'src', 'i18n'),
+          watch: true,
+        },
+      }),
+      resolvers: [
+        new QueryResolver(['lang', 'l']),
+        new HeaderResolver(['x-lang']),
+        new CookieResolver(),
+        AcceptLanguageResolver,
+      ],
+      inject: [ConfigService],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { I18nValidationExceptionFilter, I18nValidationPipe } from 'nestjs-i18n';
+
 import { AppModule } from './app.module';
+import { ErrorInterceptor } from '@/common/interceptors/error.interceptor';
+import { ResponseInterceptor } from '@/common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8080);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  app.useGlobalPipes(
+    new I18nValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.setGlobalPrefix('api/v1', { exclude: [''] }); // Exclude root path from global prefix
+  app.useGlobalFilters(new I18nValidationExceptionFilter({ detailedErrors: false }));
+  await app.listen(port || 8080);
 }
 bootstrap();
