6d1cedef16e7de075726c0523e54730d2748a838
Le Van Hai <lehai@HSVINA.LOCAL>
2025-11-08 14:12:46 +0700

    .

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,26 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { I18nValidationExceptionFilter, I18nValidationPipe } from 'nestjs-i18n';
+import { I18nValidationPipe } from 'nestjs-i18n';
+import { HttpStatus } from '@nestjs/common';
 
 import { AppModule } from './app.module';
-import { ErrorInterceptor } from '@/common/interceptors/error.interceptor';
-import { ResponseInterceptor } from '@/common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const frontendUrl = configService.get<string>('FRONTEND_URL') || '*';
   const port = configService.get('PORT');
   app.useGlobalPipes(
     new I18nValidationPipe({
       transform: true,
       whitelist: true,
       forbidNonWhitelisted: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
     }),
   );
 
-  app.useGlobalInterceptors(new ResponseInterceptor(), new ErrorInterceptor());
-  app.setGlobalPrefix('api/v1', { exclude: [''] }); // Exclude root path from global prefix
-  app.useGlobalFilters(new I18nValidationExceptionFilter({ detailedErrors: false }));
+  app.enableCors({
+    origin: frontendUrl,
+    credentials: true,
+  });
+
+  app.setGlobalPrefix('api/v1', { exclude: [''] });
   await app.listen(port || 8080);
 }
 bootstrap();
