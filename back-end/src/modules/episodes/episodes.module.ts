601038fdf6027d1e695185b6fae63e19366a5e35
Le Van Hai <lehai@HSVINA.LOCAL>
2025-11-08 07:48:41 +0700

    .

--- back-end/src/modules/episodes/episodes.module.ts
+++ back-end/src/modules/episodes/episodes.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
 import { EpisodesController } from './episodes.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Apisodes, ApisodesSchema } from './schema/episodes.schema';
+import { Apisodes, ApisodeSchema } from './schema/episodes.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Apisodes.name, schema: ApisodesSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Apisodes.name, schema: ApisodeSchema }])],
   controllers: [EpisodesController],
   providers: [EpisodesService],
 })
